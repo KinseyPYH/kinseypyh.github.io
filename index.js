ce38f3e68cfdb5787ddb2bc600d95a1e5b9341fb
KinseyPYH <kinsey@fredho.net>
2021-01-31 00:36:03 +0800

    Remodel one-pager

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).scroll(function() {
+    if ($(document).scrollTop() > 100) {
+        $('#toTop:hidden').stop(true, true).fadeIn();
+        $('#toTop').on("click", function() {
+            $(document).scrollTop(0);
+        });
+        $('#toTop').show();
+    } else {
+        $('#toTop').stop(true, true).fadeOut();
+        $('#toTop').hide();
+    }
+});
+
+
+
+
+
